b8b76a350ae242d86913ff170e1ecfaabac280ba
linzhanghui <linzhanghui@gmail.com>
2015-04-30 13:54:35 +0800

    html body

--- src/cn/zohar/jsoup/Testjsoup01.java
+++ src/cn/zohar/jsoup/Testjsoup01.java
@@ -2,12 +2,14 @@ package cn.zohar.jsoup;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 public class Testjsoup01 {
 	public static void main(String[] args){
 		String html = "<html><head><title>First parse</title></head>"
 				+ "<body><p>Parsed html into a doc.</body></html>";
-		Document doc = Jsoup.parse(html);
-		System.out.println(doc);
+		Document doc = Jsoup.parseBodyFragment(html);
+		Element body = doc.body();
+		System.out.println(body);
 	}
 }
